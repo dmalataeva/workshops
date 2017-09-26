7f51d59f7376824442cb6b82864e6b06f33782cb
dmalataeva <deniza.malataeva@gmail.com>
2017-09-26 13:56:33 -0400

    Added floaty shape and bug fix

--- webgl-workshop/src/script.js
+++ webgl-workshop/src/script.js
@@ -1,3 +1,37 @@
-import './three.js';
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+//renderer.setClearColor(0xEEEEEE);
+document.body.appendChild(renderer.domElement);
 
-console.log("SHOCKED");
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 20;
+
+
+var shape = new THREE.DodecahedronGeometry(10);
+var material = new THREE.MeshLambertMaterial();
+var mesh = new THREE.Mesh(shape, material);
+
+var ambient = new THREE.AmbientLight(0xFFFFFF, 0.1);
+var light = new THREE.DirectionalLight(0xFFFFFF, 1);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+light.position.set(-15, 15, 15);
+
+scene.add(camera);
+scene.add(mesh);
+scene.add(ambient);
+scene.add(light);
+
+animateMesh();
+
+console.log("If you see this, I'm working");
+
+function animateMesh() {
+	requestAnimationFrame(animateMesh);
+	mesh.rotation.z += 0.01;
+	render();
+}
+
+function render() {
+	renderer.render(scene, camera);
+}
