f3037355fcca5a38d7291e25633348bf1d9be876
dmalataeva <deniza.malataeva@gmail.com>
2017-10-02 14:15:09 -0400

    Added more examokmples and stereoscopic images for panorama basis

--- webgl-workshop/src/public/webgl.js
+++ webgl-workshop/src/public/webgl.js
@@ -75,20 +75,23 @@
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xEEEEEE);
 document.body.appendChild(renderer.domElement);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 20;
 
+var meshes = [];
+// loop particle creation here
 var shape = new THREE.DodecahedronBufferGeometry(10);
-var material = new THREE.MeshPhongMaterial(0xFF4D4D);
+var material = new THREE.MeshNormalMaterial();
 var mesh = new THREE.Mesh(shape, material);
 
 var ambient = new THREE.AmbientLight(0xFFFFFF, 0.1);
-var light = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+var light = new THREE.DirectionalLight(0xFFFFFF, 1);
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-light.position.set(50, 0, 10);
+light.position.set(-15, 15, 15);
 
 scene.add(camera);
 scene.add(mesh);
